873d600b7b0ae5d0f55dcbff1af14964264ddb11
johnlenfr <johnlenfr@users.noreply.github.com>
2025-07-16 23:01:16 +0200

    Update index.js

--- evo/evo_oled/extension/usr/local/etc/evo_oled/index.js
+++ evo/evo_oled/extension/usr/local/etc/evo_oled/index.js
@@ -301,15 +301,15 @@ if (this.page === "clock") return;
 		this.driver.drawLine(1, 39, 255, 39, 3, false);
 		this.driver.drawLine(185, 39, 185, 64, 3, false);
 		// network IP
-		this.driver.setCursor(15,46);
+		this.driver.setCursor(15,50);
 		this.driver.writeString(fonts.monospace ,2, (this.ip?this.ip:"No network...") ,7);
 		// Volume
 		if(this.data && this.data.volume !== null ){
 			let volstring = this.data.volume.toString();
 			if(this.data.mute === true || volstring === "0") volstring = "X";
-			this.driver.setCursor(200,50);
+			this.driver.setCursor(200,54);
 			this.driver.writeString(fonts.icons , 1 , "0" ,7); 
-			this.driver.setCursor(210,46);
+			this.driver.setCursor(210,50);
 			this.driver.writeString(fonts.monospace ,2, volstring ,7);
 		}
 		this.driver.update(true);
