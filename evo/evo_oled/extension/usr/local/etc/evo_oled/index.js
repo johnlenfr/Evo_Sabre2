a6faeef8ba5f403ff0346c031cbe0b8b4c1a2bd4
johnlenfr <johnlenfr@users.noreply.github.com>
2025-07-16 19:56:58 +0200

    Update index.js

--- evo/evo_oled/extension/usr/local/etc/evo_oled/index.js
+++ evo/evo_oled/extension/usr/local/etc/evo_oled/index.js
@@ -12,9 +12,9 @@ var DRIVER;
 
 const default_config_path = fs.readFileSync(__dirname + '/lms/default_path').toString() || "./";
 
-var TIME_BEFORE_CLOCK = 6000; // in ms
+var TIME_BEFORE_CLOCK = 10000; // in ms
 var TIME_BEFORE_SCREENSAVER = 15000; // in ms
-var TIME_BEFORE_DEEPSLEEP = 15500; // in ms
+var TIME_BEFORE_DEEPSLEEP = 100; // in ms
 var LOGO_DURATION = 2000; // in ms
 var CONTRAST = 254; // range 1-254
 var extn_exit_sleep_mode = false;
