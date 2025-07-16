e3c1fa78bdb73b545decc29fc364e992911579ed
johnlenfr <johnlenfr@users.noreply.github.com>
2025-07-16 20:25:57 +0200

    Update index.js

--- evo/evo_oled/extension/usr/local/etc/evo_oled/index.js
+++ evo/evo_oled/extension/usr/local/etc/evo_oled/index.js
@@ -12,8 +12,9 @@ var DRIVER;
 
 const default_config_path = fs.readFileSync(__dirname + '/lms/default_path').toString() || "./";
 
+
 var TIME_BEFORE_CLOCK = 15000; // in ms
-var TIME_BEFORE_SCREENSAVER = 5000; // in ms
+var TIME_BEFORE_SCREENSAVER = 25000; // in ms
 var TIME_BEFORE_DEEPSLEEP = 100; // in ms
 var LOGO_DURATION = 2000; // in ms
 var CONTRAST = 254; // range 1-254
@@ -486,17 +487,17 @@ ap_oled.prototype.handle_sleep = function(exit_sleep, nopostdisplay = false){
 				this.snake_screensaver();
 				this.idle_timeout = setTimeout(_deepsleep_,TIME_BEFORE_DEEPSLEEP);
 			}
-			
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 			let _clock_ = ()=>{
 				this.clock_mode();
 				this.idle_timeout = setTimeout(_screensaver_,TIME_BEFORE_SCREENSAVER);
 			}
-			
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////			
 			this.idle_timeout = setTimeout( _clock_ , TIME_BEFORE_CLOCK );
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 			console.log("setTimeout: ", new Date())
 
-			this.idle_timeout = setTimeout(_screensaver_,TIME_BEFORE_SCREENSAVER);
+			// this.idle_timeout = setTimeout(_screensaver_,TIME_BEFORE_SCREENSAVER);
 
 		}
 	}
