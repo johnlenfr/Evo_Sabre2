2280b0754ff9ba44e8de8c6c52ea07b4c8f6d303
johnlenfr <johnlenfr@users.noreply.github.com>
2025-07-16 21:43:04 +0200

    Update index.js

--- evo/evo_oled/extension/usr/local/etc/evo_oled/index.js
+++ evo/evo_oled/extension/usr/local/etc/evo_oled/index.js
@@ -290,11 +290,11 @@ if (this.page === "clock") return;
 		ftime = date.format(new Date(),TIME_FORMAT);
 		
 		//  function(font, size, string, color)
-		this.driver.setCursor(5, 10);
-		this.driver.writeString( fonts.monospace ,3,fdate,3);
+		this.driver.setCursor(3, 10);
+		this.driver.writeString( fonts.monospace ,3,fdate,5);
 		
-		this.driver.setCursor(165,10);
-		this.driver.writeString( fonts.monospace ,3,ftime,3);
+		this.driver.setCursor(168,10);
+		this.driver.writeString( fonts.monospace ,3,ftime,5);
 		this.driver.drawLine(1, 41, 255, 41, 5, false);
 		
 		
@@ -308,7 +308,21 @@ if (this.page === "clock") return;
 			this.driver.setCursor(195,47);
 			this.driver.writeString(fonts.icons , 1 , "0" ,4); 
 			this.driver.setCursor(205,47);
+			this.driver.writeString(fonts.monospace ,1, volstring ,10);
+			this.driver.setCursor(190,47);
+			this.driver.writeString(fonts.monospace ,1, volstring ,9);
+			this.driver.setCursor(185,47);
+			this.driver.writeString(fonts.monospace ,1, volstring ,8);
+			this.driver.setCursor(180,47);
+			this.driver.writeString(fonts.monospace ,1, volstring ,7);
+			this.driver.setCursor(175,47);
 			this.driver.writeString(fonts.monospace ,1, volstring ,6);
+			this.driver.setCursor(170,47);
+			this.driver.writeString(fonts.monospace ,1, volstring ,5);
+			this.driver.setCursor(165,47);
+			this.driver.writeString(fonts.monospace ,1, volstring ,4);
+			this.driver.setCursor(160,47);
+			this.driver.writeString(fonts.monospace ,1, volstring ,3);
 
 		}
 		this.driver.update(true);
