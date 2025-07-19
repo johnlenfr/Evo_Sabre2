52ed8c4cc928691978a56a31e75617814dcebadd
johnlenfr <johnlenfr@users.noreply.github.com>
2025-07-19 13:54:47 +0200

    Update index.js

--- evo/evo_oled/extension/usr/local/etc/evo_oled/index.js
+++ evo/evo_oled/extension/usr/local/etc/evo_oled/index.js
@@ -484,6 +484,7 @@ ap_oled.prototype.handle_sleep = function(exit_sleep, nopostdisplay = false){
 			let _screensaver_ = ()=>{
 				// this.snake_screensaver();
 				displaylogo;
+			});
 				this.idle_timeout = setTimeout(_deepsleep_,TIME_BEFORE_DEEPSLEEP);
 			}
 
