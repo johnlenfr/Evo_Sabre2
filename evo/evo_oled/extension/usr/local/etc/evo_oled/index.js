09dfb67a57725bb6ecd051eca39070abf0241086
johnlenfr <johnlenfr@users.noreply.github.com>
2025-07-22 20:41:26 +0200

    Update index.js

--- evo/evo_oled/extension/usr/local/etc/evo_oled/index.js
+++ evo/evo_oled/extension/usr/local/etc/evo_oled/index.js
@@ -179,6 +179,11 @@ ap_oled.prototype.listen_to = async function(api,frequency){
 				this.text_width = this.driver.getStringWidthUnifont(this.text_to_display + " - ");
         this.scroller_x = 0;
         this.refresh_track = REFRESH_TRACK;
+	// Met à jour les infos playlist depuis le streamer (à chaque frame)
+	if (typeof streamer !== 'undefined' && streamer.playerData) {
+	    this.playerData = streamer.playerData;
+	    this.data.playlist_cur_index = streamer.playerData.playlist_cur_index;
+	}
         this.handle_sleep(true);	
       })      
       streamer.on("volumeChange", d=>{
@@ -406,11 +411,7 @@ ap_oled.prototype.playback_mode = function(){
 	this.refresh_track = REFRESH_TRACK;
 	this.refresh_action =()=>{
 			      if(this.plotting){ return }; // skip plotting of this frame if the pi has not finished plotting the previous frame
-				// Met à jour les infos playlist depuis le streamer (à chaque frame)
-					if (typeof streamer !== 'undefined' && streamer.playerData) {
-					    this.playerData = streamer.playerData;
-					    this.data.playlist_cur_index = streamer.playerData.playlist_cur_index;
-					}	
+					
 			      this.plotting = true;
 			      this.driver.buffer.fill(0x00);
 					
